1a9e50b2ff6acfddcb33c07703b8e2de6b4ce6c9
Brandon Sotiroff <bsotiroff1@gmail.com>
2019-06-14 10:37:59 -0500

    completed updates to routes

--- server/api/orders.js
+++ server/api/orders.js
@@ -5,25 +5,12 @@ module.exports = router
 
 // getting all orders by a particular user
 // /users/:userId
-router.get('/:userId', async (req, res, next) => {
-  try {
-    const orders = await Order.findAll({
-      where: {
-        userId: req.params.userId,
-        status: 'created'
-      }
-    })
-    res.json(orders)
-  } catch (err) {
-    next(err)
-  }
-})
 
 //getting the products on a particular order
 // /orders/:orderId
-router.get('/:userId/:id', async (req, res, next) => {
+router.get('/:orderId', async (req, res, next) => {
   try {
-    const singleOrder = await Order.findByPk(req.params.id)
+    const singleOrder = await Order.findByPk(req.params.orderId)
 
     let orderProducts = await singleOrder.getProducts()
 
@@ -33,38 +20,24 @@ router.get('/:userId/:id', async (req, res, next) => {
   }
 })
 
-//add items by associating a product w/ an order
-// /users/:userId
-router.post('/:userId', async (req, res, next) => {
-  try {
-    const newOrder = await Order.create(req.body)
-    const currentUser = await User.findByPk(req.params.userId)
-    await currentUser.addOrder(newOrder)
-    res.json(newOrder)
-  } catch (error) {
-    next(error)
-  }
-})
-
 //find an order according to the userId, add a specific product to the cart. if the product is already on the cart increment the quantity
-
 // user/userId/product/productId
-router.post('/:userId/:pid', async (req, res, next) => {
+router.post('/:orderId/:productId', async (req, res, next) => {
   try {
     const orderArr = await Order.findOrCreate({
       where: {
-        userId: req.params.userId,
+        id: req.params.orderId,
         status: 'created'
       }
     })
 
     const singleOrder = orderArr[0]
 
-    const singleProduct = await Product.findByPk(req.params.pid)
+    const singleProduct = await Product.findByPk(req.params.productId)
     if (await singleOrder.hasProduct(singleProduct)) {
       let test = await ProductOrderStash.findAll({
         where: {
-          productId: req.params.pid,
+          productId: req.params.productId,
           orderId: singleOrder.id
         }
       })
@@ -81,17 +54,17 @@ router.post('/:userId/:pid', async (req, res, next) => {
 
 // delete product on an order for authorized user
 // user/userId/product/productId
-router.delete('/:userId/:pid', async (req, res, next) => {
+router.delete('/:orderId/:productId', async (req, res, next) => {
   try {
     const orderArr = await Order.findOrCreate({
       where: {
-        userId: req.params.userId,
+        id: req.params.orderId,
         status: 'created'
       }
     })
     const singleOrder = orderArr[0]
 
-    const singleProduct = await Product.findByPk(req.params.pid)
+    const singleProduct = await Product.findByPk(req.params.productId)
     await singleOrder.removeProduct(singleProduct)
 
     res.json(await singleOrder.getProducts())
@@ -105,44 +78,29 @@ router.delete('/:userId/:pid', async (req, res, next) => {
 //can we split off this find/create functionality, does it need to be on the quantity adjustment route --|
 //                                            |
 // user/:userId/product/:productId/:quantity  |
-router.put('/:userId/:pid/:num', async (req, res, next) => {
+router.put('/:orderId/:productId', async (req, res, next) => {
   try {
     const orderArr = await Order.findOrCreate({
       where: {
-        userId: req.params.userId,
+        id: req.params.orderId,
         status: 'created'
       }
     })
     const singleOrder = orderArr[0]
-    const singleProduct = await Product.findByPk(req.params.pid)
+    const singleProduct = await Product.findByPk(req.params.productId)
     if (await singleOrder.hasProduct(singleProduct)) {
       let test = await ProductOrderStash.findAll({
         where: {
-          productId: req.params.pid,
-          orderId: req.params.id
+          productId: req.params.productId,
+          orderId: req.params.orderId
         }
       })
       test[0].update({
-        quantity: Number(req.params.num)
+        quantity: Number(req.query.quantity)
       })
     }
     await singleOrder.addProduct(singleProduct)
     res.json(await singleOrder.getProducts())
-  } catch (err) {
-    next(err)
-  }
-})
-
-// checks out a cart by updating their order from 'created' to processing'
-//TODO: Add functionality to 'freeze' priceAtPurchase
-// order/:orderId
-router.put('/:userId/:id/checkout', async (req, res, next) => {
-  try {
-    const singleOrder = await Order.findByPk(req.params.id)
-    singleOrder.update({
-      status: 'processing'
-    })
-    res.json('hey good job')
   } catch (err) {
     next(err)
   }
